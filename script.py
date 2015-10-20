67c747e686f4efa56fe2f4c899288de8bd7cad46
faiq <faiqrazarizvi@gmail.com>
2015-10-20 14:58:19 -0400

    done with challenge

--- script.py
+++ script.py
@@ -1,8 +1,32 @@
 import re
+import json
 
-with open('data.in', 'r') as f:
-	out = list()
-	for line in f:
+def sanitize_phone_number(phone_number):
+	phone_number = phone_number.replace(' ', '')
+	phone_number = phone_number.replace('-', '')
+	phone_number = phone_number.replace(')', '')
+	phone_number = phone_number.replace('(', '')
+	return phone_number
+
+
+def validate_phone_number(phone_number):
+	sanitized = sanitize_phone_number(phone_number)
+	if len(sanitized) == 10:
+		return True
+	return False
+
+
+def normalize_phone_number(phone_number):
+	sanitized = sanitize_phone_number(phone_number)
+	normailzed = '%s-%s-%s' % (sanitized[:3], sanitized[3:6], sanitized[6:])
+	return normailzed
+
+
+def process_lines(lines):
+	out = dict()
+	out['entries'] = list()
+	out['errors'] = list()
+	for count, line in enumerate(lines):
 		candidate = line.rfind(',')
 		last_word = line[candidate+2:] if candidate > -1 else ''
 		user = dict()
@@ -11,12 +35,12 @@ with open('data.in', 'r') as f:
 			# zip code format is Lastname, Firstname, (703)-742-0996, Blue, 10013
 			user_keys = ['lastname', 'firstname', 'phonenumber', 'color', 'zipcode']
 			for i, key in enumerate(user_keys):
-				user[key] = fields[i].strip()
+				user[key] = fields[i].rstrip()
 		elif re.match('^([a-z])', last_word):
 			# color format is Firstname, Lastname, 10013, 646 111 0101, Green
 			user_keys = ['firstname', 'lastname', 'zipcode', 'phonenumber', 'color']
 			for i, key in enumerate(user_keys):
-				user[key] = fields[i].strip()
+				user[key] = fields[i].rstrip()
 		else:
 			# phone format is Firstname Lastname, color, 10013, 646 111 0101
 			if len(fields) > 1:
@@ -27,7 +51,25 @@ with open('data.in', 'r') as f:
 				remaining_keys = ['color', 'zipcode', 'phonenumber']
 				for i in range(1, 4):
 					key = remaining_keys[i-1]
-					user[key] = fields[i] 
-			else: 
-				print 'error', fields
-		print user
+					user[key] = fields[i].rstrip()
+		if user and validate_phone_number(user['phonenumber']):
+			user['phonenumber'] = normalize_phone_number(user['phonenumber'])
+			out['entries'].append(user)
+		else:
+			out['errors'].append(count)
+	return out
+
+
+def main():
+	processed_output = None
+	with open('data.in', 'r') as f:
+		processed_output = process_lines(f)
+	if processed_output is not None:
+		with open('result.out', 'w') as outfile:
+			json.dump(processed_output, outfile, sort_keys=True,
+				indent=2)
+	else:
+		print 'looks like something broke...'
+
+if __name__ == "__main__":
+	main()
